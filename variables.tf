1b016bd6dc5187f008b2612059b0e56ac8639762
Kranthi Katepalli <97058796+katepallikr@users.noreply.github.com>
2024-04-01 13:27:54 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,2 +1,11 @@
-variable "sg" {}
-variable "subnet" {}
+variable "sg" {
+  description = "Security group ID for the resource"
+  type        = string
+  default     = "sg-01fd57f60102b3f2b"
+}
+
+variable "subnet" {
+  description = "Subnet ID for the resource"
+  type        = string
+  default     = "subnet-0e0e06b12816aab73"
+}
